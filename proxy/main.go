033e596c2157b65c85e3ad3e5d65c0aa2a3d21e6
juli0n21 <julianschlueterbusiness21@gmail.com>
2024-12-08 16:07:36 +0100

    ignore env file

--- proxy/main.go
+++ proxy/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -11,12 +12,12 @@ func main() {
 
 	if value := os.Getenv("ENV"); value == "prod" {
 		if ok := godotenv.Load(".env"); ok != nil {
-			panic(".env not found")
+			log.Println(".env not found")
 		}
 	} else {
 		fmt.Println("Fallback to dev.env")
 		if ok := godotenv.Load("dev.env"); ok != nil {
-			panic("dev.env not found")
+			log.Println("dev.env not found, falling back to ENVIORMENT VARS")
 		}
 
 	}
