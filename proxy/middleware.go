3cfc7692691ebe0046b1d3834e1ff0caa49e2ccf
JuLi0n21 <julianschlueterbusiness21@gmail.com>
2025-03-17 23:36:13 +0100

    enhance logger

--- proxy/middleware.go
+++ proxy/middleware.go
@@ -75,7 +75,7 @@ func CORS(next http.Handler) http.Handler {
 func Logger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		fmt.Println(r.URL)
+		fmt.Println(r.Method, r.URL, r.Referer())
 		next.ServeHTTP(w, r)
 	})
 
