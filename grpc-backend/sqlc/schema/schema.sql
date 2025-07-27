841a569da795beb8cfc4afaa997ff0430ca83e05
JuLi0n21 <julianschlueterbusiness21@gmail.com>
2025-07-27 13:46:39 +0200

    ignore instead of spam cli

--- grpc-backend/sqlc/schema/schema.sql
+++ grpc-backend/sqlc/schema/schema.sql
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS Beatmap (
     Tags TEXT DEFAULT '',
     LastPlayed INTEGER DEFAULT 0,
     Folder TEXT DEFAULT 'Unknown Folder',
-    UNIQUE (Artist, Title, MD5Hash, Difficulty, Folder)
+    UNIQUE (Artist, Title, MD5Hash, Difficulty, Folder) ON CONFLICT IGNORE
 );
 
 CREATE INDEX IF NOT EXISTS idx_beatmap_md5hash ON Beatmap(MD5Hash);
